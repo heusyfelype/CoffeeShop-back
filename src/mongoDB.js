0fe119a491e524e770bb925be2fd7a0aa0932c52
heusyfelype <felype.heusy@gmail.com>
2022-05-11 11:46:56 -0300

    fix: caminho da conexão com o mongoDB

---
+++ src/mongoDB.js
@@ -0,0 +1,22 @@
+
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export let database = null;
+
+export function mongoConnection () {
+
+    const mongoClient = new MongoClient(process.env.MONGO_URL);
+    mongoClient.connect().then(() => {
+    
+        database = mongoClient.db('CoffeeShop');   
+    
+    }).catch((e) => {
+    
+        console.log(e);
+    
+    });
+
+}
